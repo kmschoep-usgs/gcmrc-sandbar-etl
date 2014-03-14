82443735b5374ff79490b14c3ace7e9c9a74e81c
kmschoep <kmschoep@IGSARMEWLTKMS.gs.doi.net>
2014-03-14 14:19:13 -0500

    added a record delimiter option, as the last column value has a newline tacked on.

--- loadCalcsProd.sql
+++ loadCalcsProd.sql
@@ -5,6 +5,26 @@ whenever sqlerror exit failure rollback;
 whenever oserror exit failure rollback;
 select 'start time: ' || systimestamp from dual;
 
+insert into area_volume_calc (
+site_id
+,sandbar_id
+,calc_date
+,volume_amt
+,calc_type
+,plane_height
+,area_2d_amt
+,area_3d_amt)
+select 
+s.id 
+,null 
+,to_date(substr(acs.dataset,instr(acs.dataset, '_', 1, 3)+1),'yyyymmdd')
+,acs.volume_amt
+,substr(acs.dataset,instr(acs.dataset, '\')+1, instr(acs.dataset, '_',1,2)-instr(acs.dataset, '\')-1)
+, ACS.PLANE_HEIGHT
+, ACS.AREA_2D_AMT 
+, ACS.AREA_3D_AMT  
+from AREA_VOLUME_CALC_stage acs, sites s
+where upper(substr(acs.dataset,instr(acs.dataset, '_', 1, 2)+1, instr(acs.dataset, '_',1,3)-instr(acs.dataset, '_', 1, 2)-1)) = S.GCMRC_SITE_ID
 
 commit;
 
